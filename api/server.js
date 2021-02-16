7549eccff35c2e53b2387d3ff64612df8298f7d2
Amanda Nelson <amanda.gaylord@gmail.com>
2021-02-16 14:54:45 -0500

    index.js and api/server.js set up

--- api/server.js
+++ api/server.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const express = require("express");
+const postsRouter = require("./posts/posts-router");
+const usersRouter = require("./users/users-router");
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
 // global middlewares and routes need to be connected here
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+server.use(express.json());
+server.use(postsRouter);
+server.use(usersRouter);
+//Unknown error catch middleware:
+server.user((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong, please try again later.",
+  });
 });
 
 module.exports = server;
